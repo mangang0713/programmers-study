abecdc941bf99bc8c456d4e086da02bd9a652545
Chae_GoWon <139374266+mangang0713@users.noreply.github.com>
2024-03-01 19:14:34 +0900

    [level 2] Title: 숫자의 표현, Time: 0.58 ms, Memory: 35.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -4,8 +4,9 @@ function solution(n) {
     
     function start(e) {
         let startNum = 0;
-        for(let i=1; i<e; i++)
+        for(let i=1; i<e; i++){
             startNum += i;
+        }
         return startNum;
     }
     
@@ -13,12 +14,28 @@ function solution(n) {
         if(!((n - start(i)) % i))
             count++;
     }
+    // n = 15 start(2) = 3  12 % 2 = 0
 
     return count;
 }
 
 /*
+15 = (1+2) + 2의배수 
+12가 2의배수냐? 맞으면 2개의 연속된 숫자로 15를 만들수 있다
 
-n개 -> 1~n더한값 + n의 배수
+
+n개 -> 1~n더한값 + n의 배수(0부터)
+
+
+1 -> 1
+2 -> 2
+3 -> 1,2 / 3
+4 -> 4
+5 -> 2,3 / 5
+6 -> 1,2,3 / 6
+7 -> 3,4 / 7
+8 -> 8
+9 -> 2,3,4 / 4,5 / 9
+10 -> 1,2,3,4 / 10
 
 */
