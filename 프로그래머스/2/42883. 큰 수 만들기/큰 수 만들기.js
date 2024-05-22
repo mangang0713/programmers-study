46d787ca05ea2d680d3bc5d31713b20bdde871ee
Chae_GoWon <139374266+mangang0713@users.noreply.github.com>
2024-05-22 16:29:18 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -13,3 +13,31 @@ function solution(number, k) {
     answer = answer.slice(0, answer.length - k);
     return answer.join('');
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function solution(number, k) {
+//     let numbering = number.split('');
+//     let answer = [];
+    
+//     for(let i=0; i<number.length; i++){
+//         answer.push(numbering.shift());
+//         while(k && (Number(numbering[0]) > Number(answer[answer.length-1]))){
+//             answer.pop();
+//             k--;
+//         }
+//     }
+//     answer.push(numbering.join(''));
+//     return answer.join('');
+// }
