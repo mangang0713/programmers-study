1e27b3e0a05777c6215509eb3e96c6149b874763
Chae_GoWon <139374266+mangang0713@users.noreply.github.com>
2025-05-02 18:08:54 +0900

    [level 2] Title: 귤 고르기, Time: 4432.68 ms, Memory: 42.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -2,16 +2,13 @@ function solution(k, tangerine) {
     let sorted = tangerine.sort((a,b) => a - b);
     let index = [0];
     let j=0;
-    console.log('sorted:',sorted);
     for(let i=1; i<=sorted.length; i++){
         if(sorted[i] > sorted[i-1] || i === sorted.length){
             index[j+1] = i-index.reduce((prev, curr) => prev + curr);
             j++;
         }
     }
-    console.log('index: ', index);
     let sortedIndex = index.sort((a,b) => b - a);
-    console.log('sortedIndex:', sortedIndex);
     let answer = 0;
     let num = 0;
     for(let i=0; i<sortedIndex.length; i++){
