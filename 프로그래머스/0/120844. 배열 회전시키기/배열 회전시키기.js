86173c1272f07dfea8720df391ed7032e22793b6
Chae_GoWon <139374266+mangang0713@users.noreply.github.com>
2025-05-30 09:32:24 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,12 @@
+function solution(numbers, direction) {
+    var answer = [];
+    if(direction === 'right'){
+        numbers.unshift(numbers[numbers.length-1])
+        numbers.pop();
+    } 
+    else{
+        numbers.push(numbers[0]);
+        numbers.shift();
+    } 
+    return numbers;
+}
