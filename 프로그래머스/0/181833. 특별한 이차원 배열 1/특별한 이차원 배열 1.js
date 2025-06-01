2567a6b48450d4550a424725feb7a05db3330a99
Chae_GoWon <139374266+mangang0713@users.noreply.github.com>
2025-06-02 08:27:19 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.64 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = [];
+    for(let i=0; i<n; i++){
+        answer.push([]);
+        for(let j=0; j<n; j++){
+            i === j ? answer[i].push(1) : answer[i].push(0);
+        }
+    }
+    return answer;
+}
